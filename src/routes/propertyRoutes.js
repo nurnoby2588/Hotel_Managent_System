034e8b2a7b5c96326dbd56c8e9358d30799545d8
nurnoby admin <nurnoby323@gmail.com>
2025-03-06 10:33:22 +0600

    route

---
+++ src/routes/propertyRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const propertyController = require('../controllers/propertyController');
+const validateProperty = require('../middleware/validateProperty');
+
+router.post('/', validateProperty, propertyController.createProperty);
+router.get('/', propertyController.getAllProperties);
+router.get('/:id', propertyController.getProperty);
+router.put('/:id', validateProperty, propertyController.updateProperty);
+router.delete('/:id', propertyController.deleteProperty);
+
+module.exports = router;
